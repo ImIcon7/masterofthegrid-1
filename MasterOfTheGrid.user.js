ecae70c25a08a8e4141e658f8c7932f5908b6ad5
geoffrey.migliacci <geoffrey.migliacci@gmail.com>
2020-01-26 18:25:33 +0100

    git init

---
+++ MasterOfTheGrid.user.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name        MasterOfTheGrid
+// @namespace   myerffoeg
+// @version     1.0.0
+// @author      Geoffrey Migliacci
+// @description A MasterOfTheGrid simple cheat.
+// @homepage    https://github.com/yerffeog/masterofthegrid
+// @icon        https://github.com/yerffeog/masterofthegrid/raw/master/MasterOfTheGrid.png
+// @updateURL   https://github.com/myerffoeg/masterofthegrid/blob/master/MasterOfTheGrid.user.js
+// @supportURL  https://github.com/yerffeog/masterofthegrid/issues
+// @match       http://masterofthegrid.sparklinlabs.com/play/*
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js
+// @run-at      document-idle
+// @grant       none
+// @noframes
+// ==/UserScript==
+
+if (localStorage.getItem('MasterOfTheGrid') === null) {
+    $.ajax({
+        method: 'GET',
+        url: 'https://raw.githubusercontent.com/myerffoeg/masterofthegrid/master/fr-FR.json',
+        cache: true,
+        dataType: 'json',
+        success: (references) => {
+            localStorage.setItem('MasterOfTheGrid', JSON.stringify(references));
+        }
+    });
+}
+
+$(document).on('keypress', (e) => {
+    if (channel.data) {
+        if (e.which === 178) {
+            const challenges = localStorage.getItem('MasterOfTheGrid') === null ? {} : JSON.parse(localStorage.getItem('MasterOfTheGrid'));
+
+            channel.socket.on('challengeInProgress', () => {
+                if (!challenges[channel.data.challenge.category]) {
+                    challenges[channel.data.challenge.category] = {};
+                }
+
+                if (challenges[channel.data.challenge.category][channel.data.challenge.id]) {
+                    if (['chooseMultiple', 'sort'].indexOf(channel.data.challenge.challengeType) !== -1) {
+                        channel.socket.emit('setAnswer', challenges[channel.data.challenge.category][channel.data.challenge.id].answer.reduce((a, c) => {
+                            a.push(channel.data.challenge.choices.indexOf(c));
+                            return a;
+                        }, []));
+                    } else if (channel.data.challenge.challengeType === 'chooseOne') {
+                        channel.socket.emit('setAnswer', channel.data.challenge.choices.indexOf(challenges[channel.data.challenge.category][channel.data.challenge.id].answer));
+                    } else if (['trueOrFalse', 'estimate'].indexOf(channel.data.challenge.challengeType) !== -1) {
+                        channel.socket.emit('setAnswer', challenges[channel.data.challenge.category][channel.data.challenge.id].answer);
+                    } else if (channel.data.challenge.challengeType === 'map') {
+                        channel.socket.emit('setAnswer', channel.data.challenge.choices.findIndex((a) => challenges[channel.data.challenge.category][channel.data.challenge.id].answer.x === a.x && challenges[channel.data.challenge.category][channel.data.challenge.id].answer.y === a.y));
+                    }
+                }
+            });
+
+            channel.socket.on('challengeAnswer', (challengeAnswer) => {
+                if (!challenges[channel.data.challenge.category]) {
+                    challenges[channel.data.challenge.category] = {};
+                }
+
+                if (!challenges[channel.data.challenge.category][channel.data.challenge.id]) {
+                    if (['chooseMultiple', 'sort'].indexOf(channel.data.challenge.challengeType) !== -1) {
+                        challenges[channel.data.challenge.category][channel.data.challenge.id] = {
+                            challengeType: channel.data.challenge.challengeType,
+                            answer: challengeAnswer.reduce((a, c) => {
+                                a.push(channel.data.challenge.choices[c]);
+                                return a;
+                            }, [])
+                        };
+                    } else if (['map', 'chooseOne'].indexOf(channel.data.challenge.challengeType) !== -1) {
+                        challenges[channel.data.challenge.category][channel.data.challenge.id] = {
+                            challengeType: channel.data.challenge.challengeType,
+                            answer: channel.data.challenge.choices[challengeAnswer]
+                        };
+                    } else if (channel.data.challenge.challengeType === 'estimate') {
+                        challenges[channel.data.challenge.category][channel.data.challenge.id] = {
+                            challengeType: channel.data.challenge.challengeType,
+                            answer: challengeAnswer.minimumValue === challengeAnswer.maximumValue ? challengeAnswer.minimumValue : Math.floor((challengeAnswer.minimumValue + challengeAnswer.maximumValue) / 2)
+                        };
+                    } else if (channel.data.challenge.challengeType === 'trueOrFalse') {
+                        challenges[channel.data.challenge.category][channel.data.challenge.id] = {
+                            challengeType: channel.data.challenge.challengeType,
+                            answer: challengeAnswer
+                        };
+                    }
+                    localStorage.setItem('MasterOfTheGrid', JSON.stringify(challenges));
+                }
+            });
+
+            $(document).unbind('keypress');
+        }
+    }
+});
---
+++ README.md
@@ -0,0 +1,15 @@
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=myerffoeg_masterofthegrid&metric=alert_status)](https://sonarcloud.io/dashboard?id=myerffoeg_masterofthegrid)
+[![Downloads](https://img.shields.io/github/downloads/myerffoeg/masterofthegrid/latest/total)](https://github.com/myerffoeg/masterofthegrid/releases/) 
+[![PayPal](https://img.shields.io/badge/Donate-PayPal-ff3f59.svg)](https://www.paypal.me/myerffoeg) 
+
+[![Twitter](https://img.shields.io/twitter/follow/myerffoeg.svg?style=social&label=Follow)](https://twitter.com/myerffoeg)
+[![Github](https://img.shields.io/github/followers/myerffoeg.svg?label=Follow&style=social)](https://github.com/myerffoeg)
+
+## Description
+A small cheat for [MasterOfTheGrid](http://masterofthegrid.sparklinlabs.com/) use the ² key to load it.
+
+## Installation
+Use a userscript manager like [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) on Google Chrome or [Greasemonkey](https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Greasemonkey.svg/220px-Greasemonkey.svg.png) on Firefox then install [MasterOfTheGrid.user.js](https://github.com/myerffoeg/masterofthegrid/releases/download/1.0.0/MasterOfTheGrid.user.js).
+
+## License
+MasterOfTheGrid is under a [MIT License](LICENSE.md).
