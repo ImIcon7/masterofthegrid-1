bd3698cdbcfb5b26208cda38aa49fd1d51e9ef9b
geoffrey.migliacci <geoffrey.migliacci@gmail.com>
2020-01-30 23:34:40 +0100

    shwoing answers when pushing ²

--- MasterOfTheGrid.user.js
+++ MasterOfTheGrid.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MasterOfTheGrid
 // @namespace   myerffoeg
-// @version     1.0.2
+// @version     1.0.3
 // @author      Geoffrey Migliacci
 // @description A MasterOfTheGrid simple cheat.
 // @homepage    https://github.com/myerffoeg/masterofthegrid
@@ -10,7 +10,6 @@
 // @supportURL  https://github.com/myerffoeg/masterofthegrid/issues
 // @match       http://masterofthegrid.sparklinlabs.com/play/*
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
-// @require     https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js
 // @run-at      document-idle
 // @grant       none
 // @noframes
@@ -33,24 +32,44 @@ $(document).on('keypress', (e) => {
         if (e.which === 178) {
             const challenges = localStorage.getItem('MasterOfTheGrid') === null ? {} : JSON.parse(localStorage.getItem('MasterOfTheGrid'));
 
-            channel.socket.on('setState', (setState) => {
-                if (setState === 'challengeInProgress') {
+            $(document).unbind('keypress');
+            $(document).on('keypress', (k) => {
+                if (k.which === 178) {
                     if (!challenges[channel.data.challenge.category]) {
                         challenges[channel.data.challenge.category] = {};
                     }
 
                     if (challenges[channel.data.challenge.category][channel.data.challenge.id]) {
-                        if (['chooseMultiple', 'sort'].indexOf(channel.data.challenge.challengeType) !== -1) {
+                        if (channel.data.challenge.challengeType === 'chooseMultiple') {
+                            const answers = [];
+                            challenges[channel.data.challenge.category][channel.data.challenge.id].answer.forEach((a) => {
+                                const answer = channel.data.challenge.choices.indexOf(a);
+                                $(`button[data-value='${answer}']:first`).parent().addClass('Selected').parent().addClass('Answered');
+                                answers.push(answer);
+                                channel.socket.emit('setAnswer', answers);
+                            });
+                        } else if(channel.data.challenge.challengeType === 'sort') {
                             channel.socket.emit('setAnswer', challenges[channel.data.challenge.category][channel.data.challenge.id].answer.reduce((a, c) => {
                                 a.push(channel.data.challenge.choices.indexOf(c));
                                 return a;
                             }, []));
+                            // @todo show answers for sort
                         } else if (channel.data.challenge.challengeType === 'chooseOne') {
-                            channel.socket.emit('setAnswer', channel.data.challenge.choices.indexOf(challenges[channel.data.challenge.category][channel.data.challenge.id].answer));
-                        } else if (['trueOrFalse', 'estimate'].indexOf(channel.data.challenge.challengeType) !== -1) {
-                            channel.socket.emit('setAnswer', challenges[channel.data.challenge.category][channel.data.challenge.id].answer);
+                            const answer = channel.data.challenge.choices.indexOf(challenges[channel.data.challenge.category][channel.data.challenge.id].answer);
+                            $(`button[data-value='${answer}']:first`).parent().addClass('Selected').parent().addClass('Answered');
+                            channel.socket.emit('setAnswer', answer);
+                        } else if (channel.data.challenge.challengeType === 'trueOrFalse') {
+                            const answer = challenges[channel.data.challenge.category][channel.data.challenge.id].answer;
+                            $(`button[data-value='${answer}']:first`).parent().addClass('Selected').parent().addClass('Answered');
+                            channel.socket.emit('setAnswer', answer);
+                        } else if (channel.data.challenge.challengeType === 'estimate') {
+                            const answer = challenges[channel.data.challenge.category][channel.data.challenge.id].answer;
+                            $('.Estimate:first').val(answer);
+                            channel.socket.emit('setAnswer', answer);
                         } else if (channel.data.challenge.challengeType === 'map') {
-                            channel.socket.emit('setAnswer', channel.data.challenge.choices.findIndex((a) => challenges[channel.data.challenge.category][channel.data.challenge.id].answer.x === a.x && challenges[channel.data.challenge.category][channel.data.challenge.id].answer.y === a.y));
+                            const answer = channel.data.challenge.choices.findIndex((a) => challenges[channel.data.challenge.category][channel.data.challenge.id].answer.x === a.x && challenges[channel.data.challenge.category][channel.data.challenge.id].answer.y === a.y);
+                            $(`button[data-value='${answer}']:first`).addClass('Active');
+                            channel.socket.emit('setAnswer', answer);
                         }
                     }
                 }
@@ -89,8 +108,6 @@ $(document).on('keypress', (e) => {
                     localStorage.setItem('MasterOfTheGrid', JSON.stringify(challenges));
                 }
             });
-
-            $(document).unbind('keypress');
         }
     }
 });
